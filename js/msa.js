24dfeb3d143566c21b4e60b1be7e0c8bdb03bcac
muhammetseyda <muhammedseyda@hotmail.com>
2024-06-28 15:54:34 +0300

    .

---
+++ js/msa.js
@@ -0,0 +1,35 @@
+const usernames = ['muhammetseyda', 'yazilimcisarman'];
+
+async function fetchRepos() {
+    try {
+        for (const username of usernames) {
+            const response = await fetch(`https://api.github.com/users/${username}/repos`);
+            const repos = await response.json();
+
+            const reposContainer = document.getElementById('repos');
+            repos.forEach(repo => {
+                if (repo.stargazers_count > 0) {
+                const repoElement = document.createElement('div');
+                repoElement.classList.add('grid-list-items__item', 'list-items__item');
+                repoElement.style.marginBottom = '100px'; 
+                var reponame = repo.name.toUpperCase();
+                repoElement.innerHTML = `
+                    <div class="list-items__item-header">
+                        <h3 class="list-items__item-title"><a href="${repo.html_url}" style="color:black;" target="_blank">${reponame}</a></h3>
+                    </div>
+                    <div class="list-items__item-text">
+                        <p>
+                        ${repo.description ? repo.description : 'Açıklama yok.'}
+                        </p>
+                    </div>
+                    <a href="${repo.html_url}" target="_blank">Repo Link</a>
+                `;
+                reposContainer.appendChild(repoElement);}
+            });
+        }
+    } catch (error) {
+        console.error('Error fetching repos:', error);
+    }
+}
+
+fetchRepos();
